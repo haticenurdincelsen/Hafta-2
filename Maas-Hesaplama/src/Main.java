64eefd47a5c5ccdf519381c609454ab6a5592b78
haticenur <haticenur_dincel@hotmail.com>
2023-09-24 13:26:33 +0300

    ortalama

--- Maas-Hesaplama/src/Main.java
+++ Maas-Hesaplama/src/Main.java
@@ -1,14 +1,13 @@
 
-public class Main {
-    public static void main(String[] args) {
-        //personelleri oluşturalım .
-        //p1 ve p2 adında personel oluşturalım.
-        Employee p2=new Employee("Haticenur DİNÇEL ŞEN",2000.0,55,1996);
-        Employee p1=new Employee("Kemal",2000.0,45,1985);
 
-        p1.toStringCalculate();
-        System.out.println("=====================");
-        p2.toStringCalculate();
+                public class Main {
+                    public static void main(String[] args) {
+                        int[] scores = new int[10];
+                        scores = new int[3];
+                        scores = new int[]{215, 234, 218, 189, 221, 290};
 
-        }
-    }
+                        for (int score : scores) {
+                            System.out.print(score + "  ");
+                        }
+                    }
+                }
